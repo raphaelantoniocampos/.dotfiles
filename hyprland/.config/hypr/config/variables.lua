ffaab1fd97bb424b8af42ce01ca344eb303833f1
Raphael Campos <raphael.antonio.campos@proton.me>
2026-08-17 23:42:32 -0300

    hyprland: updating to lua

---
+++ hyprland/.config/hypr/config/variables.lua
@@ -0,0 +1,15 @@
+-- Hyprland default apps
+
+TERMINAL = "kitty"
+FILE_MANAGER = "dolphin"
+BROWSER = "firefox"
+CALCULATOR = "gnome-calculator"
+SCRIPTS_DIR = "$HOME/.scripts/"
+
+-- Monitors
+MONITOR1 = "eDP-1"
+MONITOR2 = "HDMI-A-1"
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 10 -- Number of workspaces per monitor (Max 10)
